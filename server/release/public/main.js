71d1fb1275450e7cec9c1dada0079b021d94a95b
Felix Hageloh <felix.hageloh@gmail.com>
2014-08-12 12:36:14 +0200

    Not failing if  default port is in use.
    Checking 10 other ports before giving up.
    Refactord the server task to use the more modern block syntax.

--- server/release/public/main.js
+++ server/release/public/main.js
@@ -184,9 +184,10 @@ module.exports = function(port, widgetPath) {
   widgetPath = path.resolve(__dirname, widgetPath);
   widgetDir = WidgetDir(widgetPath);
   changesServer = ChangesServer();
-  server = connect().use(connect["static"](path.resolve(__dirname, './public'))).use(WidgetCommandServer(widgetDir)).use(WidgetsServer(widgetDir)).use(changesServer.middleware).use(connect["static"](widgetPath)).listen(port);
-  widgetDir.onChange(changesServer.push);
-  console.log('server started on port', port);
+  server = connect().use(connect["static"](path.resolve(__dirname, './public'))).use(WidgetCommandServer(widgetDir)).use(WidgetsServer(widgetDir)).use(changesServer.middleware).use(connect["static"](widgetPath)).listen(port, function() {
+    console.log('server started on port', port);
+    return widgetDir.watch(changesServer.push);
+  });
   return server;
 };
 
@@ -568,12 +569,13 @@ loader = require('./widget_loader.coffee');
 paths = require('path');
 
 module.exports = function(directoryPath) {
-  var api, changeCallback, deleteWidget, init, isWidgetPath, loadWidget, notifyChange, notifyError, prettyPrintError, registerWidget, watcher, widgetId, widgets;
+  var api, changeCallback, deleteWidget, init, isWidgetPath, loadWidget, notifyChange, notifyError, prettyPrintError, registerWidget, widgetId, widgets;
   api = {};
   widgets = {};
-  watcher = require('chokidar').watch(directoryPath);
   changeCallback = function() {};
   init = function() {
+    var watcher;
+    watcher = require('chokidar').watch(directoryPath);
     watcher.on('change', function(filePath) {
       if (isWidgetPath(filePath)) {
         return registerWidget(loadWidget(filePath));
@@ -590,14 +592,15 @@ module.exports = function(directoryPath) {
     console.log('watching', directoryPath);
     return api;
   };
+  api.watch = function(callback) {
+    changeCallback = callback;
+    return init();
+  };
   api.widgets = function() {
     return widgets;
   };
   api.get = function(id) {
     return widgets[id];
-  };
-  api.onChange = function(callback) {
-    return changeCallback = callback;
   };
   api.path = directoryPath;
   loadWidget = function(filePath) {
@@ -670,7 +673,7 @@ module.exports = function(directoryPath) {
     var _ref;
     return (_ref = filePath.match(/\.coffee$/)) != null ? _ref : filePath.match(/\.js$/);
   };
-  return init();
+  return api;
 };
 
 
--- server/server.coffee
+++ server/server.coffee
@@ -1,13 +1,17 @@
 parseArgs        = require 'minimist'
 UebersichtServer = require './src/app.coffee'
 
+
+handleError = (e) ->
+  console.log 'error:', e.message
+
 try
   args       = parseArgs process.argv.slice(2)
   widgetPath = args.d ? args.dir  ? './widgets'
   port       = args.p ? args.port ? 41416
 
-  UebersichtServer Number(port), widgetPath
+  server = UebersichtServer Number(port), widgetPath
+  server.on 'error', handleError
 catch e
-  console.log e
-  process.exit 1
+  handleError e
 
