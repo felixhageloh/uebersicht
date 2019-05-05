3502ed29f4c86ce4f102eec307b08a93f8dc474b
Felix Hageloh <felix.hageloh@gmail.com>
2019-05-05 16:06:05 +0200

    Remove debug statement and clean up code

--- server/src/runShellCommand.js
+++ server/src/runShellCommand.js
@@ -1,23 +1,22 @@
-const post = require("superagent").post;
+const post = require('superagent').post;
 
 function wrapError(err, res) {
-  if (err) console.log("wrap", err.name, err.message);
-  return err ? new Error((res || {}).text || "error running command") : null;
+  return err ? new Error((res || {}).text || 'error running command') : null;
 }
 
 function isKeepAliveError(err) {
-  return err && err.message.indexOf("Request has been terminated") === 0;
+  return err && err.message.indexOf('Request has been terminated') === 0;
 }
 
 module.exports = function runShellCommand(command, callback) {
-  const request = post("/run/")
+  const request = post('/run/')
     .retry(2, isKeepAliveError)
     .send(command);
   return callback
     ? request.end((err, res) => callback(wrapError(err, res), (res || {}).text))
     : request
-        .catch(err => {
+        .catch((err) => {
           throw wrapError(err, err.response);
         })
-        .then(res => res.text);
+        .then((res) => res.text);
 };
