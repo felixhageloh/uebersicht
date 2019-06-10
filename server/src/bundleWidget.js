cc39d8b93df66ed233b8146ccbb807aa5e9ad557
Felix Hageloh <felix.hageloh@gmail.com>
2019-06-10 19:58:20 +0200

    Upgrade all babel

--- server/src/bundleWidget.js
+++ server/src/bundleWidget.js
@@ -5,11 +5,10 @@ const watchify = require('./watchify');
 const widgetify = require('./widgetify');
 const coffeeify = require('coffeeify');
 const babelify = require('babelify');
-const jsxTransform = require('babel-plugin-transform-react-jsx');
-const restSpreadTransform = require('babel-plugin-transform-object-rest-spread');
-const envPreset = require('babel-preset-env');
+const jsxTransform = require('@babel/preset-react');
+const restSpreadTransform = require('@babel/plugin-proposal-object-rest-spread');
+const envPreset = require('@babel/preset-env');
 const through = require('through2');
-const emotionPlugin = require('babel-plugin-emotion');
 
 function wrapJSWidget() {
   let start = true;
@@ -38,7 +37,7 @@ module.exports = function bundleWidget(id, filePath) {
   });
 
   bundle.plugin(watchify);
-  bundle.require(filePath, { expose: id });
+  bundle.require(filePath, {expose: id});
   bundle.external('uebersicht');
 
   if (filePath.match(/\.coffee$/)) {
@@ -46,18 +45,18 @@ module.exports = function bundleWidget(id, filePath) {
       bare: true,
       header: false,
     });
-    bundle.transform(widgetify, { id: id });
+    bundle.transform(widgetify, {id: id});
   } else if (isJsxWidget) {
     bundle.transform(babelify, {
-      presets: [envPreset],
-      plugins: [
-        restSpreadTransform,
-        [jsxTransform, { pragma: 'html' }],
+      presets: [
+        [envPreset, {targets: 'last 4 Safari versions', modules: 'commonjs'}],
+        [jsxTransform, {pragma: 'html'}],
       ],
+      plugins: [restSpreadTransform],
     });
   } else {
     bundle.transform(wrapJSWidget);
-    bundle.transform(widgetify, { id: id });
+    bundle.transform(widgetify, {id: id});
   }
   return bundle;
 };
