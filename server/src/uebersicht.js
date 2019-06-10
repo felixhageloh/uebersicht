4fd932821357febdb77f002243de68971ab1428f
Felix Hageloh <felix.hageloh@gmail.com>
2019-06-10 15:36:29 +0200

    Expose React

--- server/src/uebersicht.js
+++ server/src/uebersicht.js
@@ -1,5 +1,6 @@
 import run from './runShellCommand';
 import request from 'superagent';
 import styled, {css} from 'react-emotion';
+import React from 'react';
 
-export {run, request, css, styled};
+export {run, request, css, styled, React};
