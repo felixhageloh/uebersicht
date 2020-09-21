c01286b1b1bea450aaa059f74cfb3e86b1042425
Felix Hageloh <felix.hageloh@gmail.com>
2020-09-21 19:52:01 +0200

    Add sponsors list

---
+++ bin/build-sponsor-links.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const sponsors = require("../sponsors/September-2020.json");
+
+sponsors.forEach((sponsor) => {
+  console.log(
+    `<a href="https://github.com/${sponsor.sponsor_handle}" title="${sponsor.sponsor_profile_name}">`
+  );
+  console.log(
+    `  <img src="https://github.com/${sponsor.sponsor_handle}.png"/>`
+  );
+  console.log(`</a>`);
+});
